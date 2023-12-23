681584e3cf37efc638257fb9b9ddf5fa7f6e51df
sametj <mosesjames2000@gmail.com>
2023-12-23 00:01:40 -0500

    Pomodoro Web App

---
+++ script.js
@@ -0,0 +1,184 @@
+    const start = document.getElementById('start-button');
+    const reset = document.getElementById('reset-button');
+    const add = document.getElementById('task-button');
+
+    const taskList = document.getElementById('current-task-container');
+    const currentTask = document.getElementById('current-task-header');
+    const currentTaskContainer = document.getElementById('current-task-div');
+    const timer = document.getElementById('timer');
+    const alert = document.getElementById('alert-text');
+    const timerContainer = document.getElementById('timer-container');
+    const timerText = document.getElementById('timer-text');
+
+    
+    
+    let count = 0;
+
+    const task = [];
+
+
+    let timerStarted = false;
+    let paused = false;
+
+    const workTimer = 1500;
+    const shortBreak = 300;
+    const longBreak = 900;
+
+    let pomodoro = ['Work Time!', 'Short Break!', 'Work Time!', 'Short Break!', 'Work Time!', 'Short Break!', 'Work Time!', 'Long Break!'];
+    //Adding tasks
+    add.addEventListener('click', function() {
+        const taskInput = document.getElementById('task-input').value;
+        if (taskInput === task[0] && timerStarted === false) {
+            alert.innerHTML = 'Task already added! Start Timer to begin!';
+            setTimeout(function() {
+                alert.innerHTML = '';
+            }, 3000);
+        }else
+        if (taskInput === task[0] && timerStarted === true) {
+            alert.innerHTML = 'Task already added and timer is running!';
+            setTimeout(function() {
+                alert.innerHTML = '';
+            }, 3000);
+        }
+            else if (taskInput === '') {
+            alert.innerHTML = 'Please enter a task!';
+            setTimeout(function() {
+                alert.innerHTML = '';
+            }, 3000);
+            alert.style.color = 'orange';
+            alert.style.fontSize = '50px';
+            return;
+        }
+        else if (taskInput !== task[0] && timerStarted === true) {
+
+            alert.innerHTML = 'Timer is already running! Reset timer to Change task!';
+            setTimeout(function() {
+                alert.innerHTML = '';
+            }, 3000);
+            alert.style.color = 'red';
+            alert.style.fontSize = '50px';
+        }
+        else{
+            task[0] = taskInput;
+            
+            taskList.innerHTML = task;
+            alert.innerHTML = 'Task Added!';
+            setTimeout(function() {
+                alert.innerHTML = '';
+            }, 3000);
+            alert.style.color = 'green';
+            alert.style.fontSize = '50px';
+        }
+        
+
+        
+    });
+
+    function startTimer(pomodoroTime) {   
+        timer.innerHTML = `<h1 id ="timer-text">${Math.floor(pomodoroTime/60)}:00</h1>`;  
+        let time = pomodoroTime;
+        const countDown = setInterval(function() {
+            time--;
+            currentTime = time;
+            const minutes = Math.floor(time / 60);
+            const seconds = time % 60;
+            timer.innerHTML = `<h1 id ="timer-text">${minutes}:${seconds < 10 ? '0' : ''}${seconds}</h1>`;
+            timerStarted = true;
+            if (time === 0) {
+                clearInterval(countDown);
+                timer.innerHTML = `<h1 id ="timer-text">${minutes}:${seconds < 10 ? '0' : ''}${seconds} </h1>`;
+                currentTask.innerHTML = pomodoro[count];
+                if (count === 7) {
+                    currentTask.innerHTML = 'Pomodoro Complete!, Click Reset to Start Again!'
+                    currentTask.style.color = 'Green';
+                }
+                timerStarted = false;
+            }
+        }, 1000);
+        
+        reset.addEventListener('click', function() {
+                timerStarted = false;
+                count = 0;
+                currentTask.innerHTML = 'Pomodoro Timer Resetted!';
+                timer.innerHTML = '<h1>00:00</h1>';
+                currentTaskContainer.style.backgroundColor = 'black';
+                currentTask.style.color = 'yellow';
+                clearInterval(countDown);
+                
+         });
+}
+
+   //Stop timer
+   
+
+   function getPomodoroStage(count){
+    switch (count){
+        case 0:
+            return currentTask.innerHTML = 'Work Time!', currentTask.style.color = 'red',currentTaskContainer.style.backgroundColor = 'black',
+            timerStarted = true,startTimer(workTimer);
+            
+        case 1: 
+            return currentTask.innerHTML = 'Short Break!', currentTask.style.color = 'yellow',currentTaskContainer.style.backgroundColor = 'black',
+            timerStarted = true,startTimer(shortBreak);
+            break;
+        case 2:
+            return currentTask.innerHTML = 'Work Time!', currentTask.style.color = 'red',currentTaskContainer.style.backgroundColor = 'black',
+            timerStarted = true,startTimer(workTimer);
+            break;
+        case 3:
+            return currentTask.innerHTML = 'Short Break!', currentTask.style.color = 'yellow',currentTaskContainer.style.backgroundColor = 'black',
+            timerStarted = true,startTimer(shortBreak);
+            break;
+        case 4:
+            return currentTask.innerHTML = 'Work Time!', currentTask.style.color = 'red',currentTaskContainer.style.backgroundColor = 'black',
+            timerStarted = true,startTimer(workTimer);
+            break;
+        case 5:
+            return currentTask.innerHTML = 'Short Break!', currentTask.style.color = 'yellow',currentTaskContainer.style.backgroundColor = 'black',
+            timerStarted = true,startTimer(shortBreak);
+            break;
+        case 6:
+            return currentTask.innerHTML = 'Work Time!', currentTask.style.color = 'red',currentTaskContainer.style.backgroundColor = 'black',
+            timerStarted = true,startTimer(workTimer);
+            break;
+        case 7:
+            return currentTask.innerHTML = 'Long Break!', currentTask.style.color = 'green',currentTaskContainer.style.backgroundColor = 'black', 
+            timerStarted = true ,startTimer(longBreak);
+            break;
+    }
+   }
+
+    start.addEventListener('click', function() {
+        if (timerStarted === false && task[0] !== undefined) {
+            timerStarted = true;
+            getPomodoroStage(count);
+            timer.style.borderColor = 'red';
+            timerText.style.color = 'red';
+            setTimeout(() => {
+                timerText.style.color = 'white';
+            }, 3000);
+            count++;
+            timerStarted.style.borderColor = 'Green';
+        }
+        else if (timerStarted === true) {
+            alert.innerHTML = 'Timer is already running!';
+            setTimeout(function() {
+                alert.innerHTML = '';
+            }, 2000);
+            alert.style.color = 'orange';
+            alert.style.fontSize = '50px';
+        }else if (task[0] === undefined){
+            alert.innerHTML = 'Please add a task!';
+            setTimeout(function() {
+                alert.innerHTML = '';
+            }, 2000);
+            alert.style.color = 'orange';
+            alert.style.fontSize = '50px';
+        }
+      
+     });
+
+
+    
+
+     
